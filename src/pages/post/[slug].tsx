0da00ee545e2616f2539c4933bb7f842e80221a9
dinobergviana <dinobergueviana@gmail.com>
2022-03-18 02:08:17 -0300

    feat: Cria pagina para um unico post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,45 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(post: PostProps): JSX.Element {
+  return (
+    <>
+      <Header />
+      <img src="/teste.png" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Exemplo</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                18 Mar 2022
+              </li>
+              <li>
+                <FiUser />
+                Dinobergue Viana
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Titulo</h2>
+            <p>
+              Lorem ipsum
+              <strong> dolor</strong> sit amet consectetur adipisicing elit.
+              Tempore, dolorum tenetur? Tenetur suscipit commodi impedit,
+              ducimus eos corrupti veniam numquam optio est aliquid, dolorem
+              nisi itaque, dolores eveniet. Error, delectus?
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
