67ba7301bda119a1ef0a04fc182d21357507d02d
dinobergviana <dinobergueviana@gmail.com>
2022-03-20 00:53:21 -0300

    feat: Configura arquivos de preview e exit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
@@ -14,6 +16,11 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=space-travelling-dino"
+          />
         </body>
       </Html>
     );
