199de4bf1ff36abbf3c69faf99c1278d8cd4c9e3
dinobergviana <dinobergueviana@gmail.com>
2022-03-19 23:58:16 -0300

    feat: Finaliza pagina de posts

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,13 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+import Prismic from '@prismicio/client';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 import Header from '../../components/Header';
 
@@ -28,41 +37,68 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(post: PostProps): JSX.Element {
-  console.log(post);
+export default function Post({ post }: PostProps): JSX.Element {
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+    return total;
+  }, 0);
+
+  const readingTime = Math.ceil(totalWords / 200);
+
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
+
+  const formatedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
   return (
     <>
+      <Head>
+        <title>{post.data.title} | spacetravelling</title>
+      </Head>
       <Header />
-      <img src="/teste.png" alt="imagem" className={styles.banner} />
+      <img src={post.data.banner.url} alt="imagem" className={styles.banner} />
       <main className={commonStyles.container}>
         <div className={styles.post}>
           <div className={styles.postTop}>
-            <h1>Exemplo</h1>
+            <h1>{post.data.title}</h1>
             <ul>
               <li>
                 <FiCalendar />
-                18 Mar 2022
+                {formatedDate}
               </li>
               <li>
                 <FiUser />
-                Dinobergue Viana
+                {post.data.author}
               </li>
               <li>
-                <FiClock />5 min
+                <FiClock /> {`${readingTime} min`}
               </li>
             </ul>
           </div>
-
-          <article>
-            <h2>Titulo</h2>
-            <p>
-              Lorem ipsum
-              <strong> dolor</strong> sit amet consectetur adipisicing elit.
-              Tempore, dolorum tenetur? Tenetur suscipit commodi impedit,
-              ducimus eos corrupti veniam numquam optio est aliquid, dolorem
-              nisi itaque, dolores eveniet. Error, delectus?
-            </p>
-          </article>
+          {post.data.content.map(content => {
+            return (
+              <article key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  className={styles.postContent}
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </article>
+            );
+          })}
         </div>
       </main>
     </>
@@ -70,11 +106,21 @@ export default function Post(post: PostProps): JSX.Element {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // const prismic = getPrismicClient();
-  // const posts = await prismic.query(TODO);
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
+
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
   return {
-    paths: [],
+    paths,
     fallback: true,
   };
 };
@@ -96,7 +142,7 @@ export const getStaticProps: GetStaticProps = async context => {
       },
       content: response.data.content.map(content => {
         return {
-          heading: content.body,
+          heading: content.heading,
           body: [...content.body],
         };
       }),
