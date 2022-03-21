f91d1ddd136bd89a89fe61285395e4d3e74f97be
dinobergviana <dinobergueviana@gmail.com>
2022-03-20 23:32:53 -0300

    feat: Implementa area de comentarios na pagina Post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -16,6 +16,7 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
@@ -102,6 +103,9 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             );
           })}
         </div>
+
+        <Comments />
+
         {preview && (
           <aside>
             <Link href="/api/exit-preview">
